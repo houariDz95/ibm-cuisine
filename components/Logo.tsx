4cb438629b1ca1f189d31fbc2aa1244752289b74
eddDevDz95 <houedd608@gmail.com>
2024-09-15 20:45:09 +0100

    new commit

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Logo = () => {
   return (
-    <Link href="/admine" className="text-light-200 text-xl italic font-medium tracking-wide">
+    <Link href="/admin" className="text-light-200 text-xl italic font-medium tracking-wide">
         <span className="text-green-500">IBM</span> Cuisine
     </Link>
   )
